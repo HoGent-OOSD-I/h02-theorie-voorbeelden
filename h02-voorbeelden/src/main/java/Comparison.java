b67f2fc522a3ed1416b6a6a8a97aa885ebfb7804
hr047 <heidi.roobrouck@hogent.be>
2026-09-14 12:04:56 +0200

    cursus juistzetten figuren

--- h02-voorbeelden/src/main/java/Comparison.java
+++ h02-voorbeelden/src/main/java/Comparison.java
@@ -2,8 +2,8 @@ void main() {
     int number1 = Integer.parseInt(IO.readln("Enter first integer: "));
     int number2 = Integer.parseInt(IO.readln("Enter second integer: "));
 
-    if (number1 == number2) {
-        IO.println(String.format("%d == %d", number1, number2));
+    if (number1 == number2) { // <1>
+        IO.println(String.format("%d == %d", number1, number2)); // <2>
     }
 
     if (number1 != number2) {
